18fd4a15b758ff35f4292d06c2fe3d9cb70caf77
jasper <jasperbinda@gmail.com>
2021-02-03 16:23:23 +0100

    remove template stuff, add name

---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <div class="name">
+      <img src="../assets/img/name.png" class="name-img">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.name {
+  display: flex;
+  width: 99vw;
+}
+.name-img {
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 90%;
+  filter:drop-shadow(2px 2px 2px white);
+}
+
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="navbar">
+    <h1>my Nav</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.navbar {
+  display: flex;
+  width: 99vw;
+}
+
+</style>
